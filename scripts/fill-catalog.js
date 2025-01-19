1c6b27889eaec968018b38b99ade42f97805e6ae
lCaba1 <a.m.kabashov@gmail.com>
2025-01-19 21:20:53 +0300

    change cards filling to template-based

--- scripts/fill-catalog.js
+++ scripts/fill-catalog.js
@@ -1,132 +1,42 @@
 const api_url = 'https://edu.std-900.ist.mospolytech.ru';
-const goods_path = '/exam-2024-1/api/goods';
+const catalog_path = '/exam-2024-1/api/goods';
 const auth = '?api_key=12fb475f-4ad1-4629-8a9b-1dbfd9e253dd';
 const page = '&page=1&per_page=3' // &sort_order=
 
-function fillGoodsCard(goods) {
-    current_element = this;
-    current_element.appendChild(
-        Object.assign(document.createElement('img'), {
-            src: goods.image_url,
-            alt: goods.id + goods.name,
-            className: 'overflow-hidden h-100 w-100 card-img-top',
-            style: 'flex-basis: auto'
-        })
-    )
-    current_element = current_element.appendChild(
-        Object.assign(document.createElement('div'), {
-            className: 'card-body',
-        })
-    );
+function createCard(goods) {
+    const card = document.getElementById('card-template').content.cloneNode(true);
 
-    current_element.appendChild(
-        Object.assign(document.createElement('h6'), {
-            className: 'card-title', // text-truncate
-            textContent: goods.name,
-            style: 'overflow: hidden; text-overflow: ellipsis; height: 2.4em; display: -webkit-box; -webkit-line-clamp: 2; line-clamp: 2; -webkit-box-orient: vertical;'
-        })
-    );
-    current_element = current_element.appendChild(
-        Object.assign(document.createElement('div'), {
-            className: 'd-flex gap-3 card-text text-muted'
-        })
-    );
+    card.querySelector('img.js_image_url').src = goods.image_url;
+    card.querySelector('h6.js_name').textContent = goods.name;
+    card.querySelector('span.js_rating').textContent = goods.rating;
 
-    current_element.appendChild(
-        Object.assign(document.createElement('span'), {
-            textContent: goods.rating.toFixed(1)
-        })
-    );
-    current_element = current_element.appendChild(document.createElement('span'));
-
-    for (let i = 1; i <= 5; i++) {
-        if (i <= goods.rating) {
-            current_element.appendChild(
-                Object.assign(document.createElement('i'), {
-                    className: 'bi bi-star-fill text-warning'
-                })
-            );
-        } else if (i - goods.rating <= 0.5) {
-            current_element.appendChild(
-                Object.assign(document.createElement('i'), {
-                    className: 'bi bi-star-half text-warning'
-                })
-            );
-        } else {
-            current_element.appendChild(
-                Object.assign(document.createElement('i'), {
-                    className: 'bi bi-star'
-                })
-            );
-        }
+    const stars = card.querySelector('span.js_rating_stars').querySelectorAll('i');
+    for (let i = 0; i < 5; i++) {
+        if (i <= goods.rating - 1) stars[i].classList.add('bi', 'bi-star-fill', 'text-warning');
+        else if (i - goods.rating + 1 <= 0.5) stars[i].classList.add('bi', 'bi-star-half', 'text-warning');
+        else stars[i].classList.add('bi', 'bi-star');
     }
 
-    current_element = current_element.parentElement.parentElement.appendChild(
-        Object.assign(document.createElement('div'), {
-            className: 'card-text d-flex justify-content-between'
-        })
-    )
-
-    current_element.appendChild(
-        Object.assign(document.createElement('span'), {
-            textContent: goods.actual_price + '\u20BD'
-        })
-    )
-    if (goods.discount_price != null) {
-        current_element.appendChild(
-            Object.assign(document.createElement('s'), {
-                className: 'text-danger',
-                textContent: goods.discount_price + '\u20BD'
-            })
-        )
-        current_element.appendChild(
-            Object.assign(document.createElement('strong'), {
-                className: 'text-danger',
-                textContent: '-' + ((goods.actual_price - goods.discount_price) / goods.actual_price * 100).toFixed(2) + '%'
-            })
-        )
+    if (goods.discount_price) {
+        card.querySelector('span.js_discount_price').textContent = goods.discount_price;
+        card.querySelector('s.js_actual_price').textContent = goods.actual_price;
+        card.querySelector('strong.js_percent_off').textContent =
+            '-' + ((1 - goods.discount_price / goods.actual_price) * 100).toPrecision(2) + '%';
+    } else {
+        card.querySelector('span.js_discount_price').textContent = goods.actual_price;
     }
 
-    current_element.parentElement.appendChild(
-        Object.assign(document.createElement('button'), {
-            className: 'btn btn-primary w-100 mt-1',
-            textContent: 'Добавить'
-        })
-    )
+    document.getElementById('catalog').appendChild(card);
 }
 
-function createCardsGrid(data) {
-    const catalog = document.getElementById('catalog');
+async function fetchCatalog() {
+    const response = await fetch(api_url + catalog_path + auth/* + page*/, { method: 'GET' });
+    const data = await response.json();
+    //console.log(data);
     data.forEach(goods => {
-        let current_element = catalog;
-        catalog.appendChild(
-            Object.assign(document.createElement('div'), {
-                className: 'col'
-            })
-        );
-        current_element = current_element.lastElementChild;
-        current_element.appendChild(
-            Object.assign(document.createElement('div'), {
-                className: 'ratio',
-                style: '--bs-aspect-ratio: 178%'
-            })
-        );
-        current_element = current_element.lastElementChild;
-        current_element.appendChild(
-            Object.assign(document.createElement('div'), {
-                className: 'card d-flex flex-column overflow-hidden'
-            })
-        );
-        current_element = current_element.lastElementChild;
-        fillGoodsCard.call(current_element, goods);
+        createCard(goods);
     });
 }
 
-async function fetchGoodsCatalog() {
-    const response = await fetch(api_url + goods_path + auth/* + page*/, { method: 'GET' });
-    const data = await response.json();
-    //console.log(data);
-    createCardsGrid(data/*.goods*/);
-}
+fetchCatalog();
 
-fetchGoodsCatalog();
