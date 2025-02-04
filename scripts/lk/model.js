9a686936369c09cb4d73579115738ad980a98ce7
lCaba1 <a.m.kabashov@gmail.com>
2025-02-04 10:19:10 +0300

    separate scripts

---
+++ scripts/lk/model.js
@@ -0,0 +1,8 @@
+const api_url = 'https://edu.std-900.ist.mospolytech.ru';
+const auth = '?api_key=12fb475f-4ad1-4629-8a9b-1dbfd9e253dd';
+const order_path = '/exam-2024-1/api/orders';
+
+export async function getOrders() {
+    const request = api_url + order_path + auth;
+    return await (await fetch(request, { method: 'GET' })).json();
+}
